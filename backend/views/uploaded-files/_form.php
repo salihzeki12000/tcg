8f24da8cb00587911b07b64fcf00cc9e3cbad62e
mixiaobin <mixiaobin@rong360ad.com>
2016-11-20 23:52:01 +0800

    test

--- backend/views/uploaded-files/_form.php
+++ backend/views/uploaded-files/_form.php
@@ -21,7 +21,6 @@ use yii\widgets\ActiveForm;
 
     <!-- <?//= $form->field($model, 'create_time')->textInput() ?> -->
 
-
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? Yii::t('app', 'Create') : Yii::t('app', 'Update'), ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
     </div>
