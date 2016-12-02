10b19586042d6b77b801a8624ffd361386723a81
mixiaobin <mixiaobin@rong360ad.com>
2016-12-02 16:33:42 +0800

    u

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -71,9 +71,9 @@ AppAsset::register($this);
 
 <footer class="footer">
     <div class="container">
-        <p class="pull-left">&copy; My Company <?= date('Y') ?></p>
+        <p class="pull-left">&copy; The China Guide <?= date('Y') ?></p>
 
-        <p class="pull-right"><?= Yii::powered() ?></p>
+        <p class="pull-right"><?//= Yii::powered() ?></p>
     </div>
 </footer>
 
