e193ddb483c44c2c088a3a673d7654bbac466b63
mixiaobin <mixiaobin@rong360ad.com>
2016-12-02 15:56:26 +0800

    u

--- backend/components/UserRule.php
+++ backend/components/UserRule.php
@@ -8,10 +8,11 @@ class UserRule extends \yii\rbac\Rule
 
     public function execute($user, $item, $params)
     {
-        if (Yii::$app->controller->action->id !== 'update' || Yii::$app->user->id == $params['id']) {
-            return true;
-        }
-        return false;
+        // if (Yii::$app->controller->action->id !== 'update' || Yii::$app->user->id == $params['id']) {
+        //     return true;
+        // }
+        // return false;
+        return true;
     }
 }
 
