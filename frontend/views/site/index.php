f699200e5ab31bff47b14ecda0690f2fd513bbc4
mixiaobin <mixiaobin@rong360ad.com>
2016-12-02 16:29:40 +0800

    u

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -115,13 +115,6 @@ $this->title = 'The China Guide';
         </div>
     </div>
 
-    <div class="jumbotron">
-        <h1>Congratulations!</h1>
-
-        <p class="lead">You have successfully created your Yii-powered application.</p>
-
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
-    </div>
 
     <div class="body-content">
 
