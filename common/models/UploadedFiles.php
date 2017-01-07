c3226a504f766fe1495c834b8166067791f11bd5
mixiaobin <mixiaobin@rong360ad.com>
2017-01-07 15:07:27 +0800

    up

--- common/models/UploadedFiles.php
+++ common/models/UploadedFiles.php
@@ -74,7 +74,7 @@ class UploadedFiles extends \yii\db\ActiveRecord
 
     static public function uploadFile($file, $use_mobile=0)
     {
-        $quality = 100;
+        $quality = 90;
         if (!empty($file))
         {
             $file_size = 0;
