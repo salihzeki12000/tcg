5620dc6382b31b5c3bd852d86c5589313b0f0b84
mixiaobin <mixiaobin@rong360ad.com>
2017-01-08 12:08:49 +0800

    up

--- frontend/views/experience/index.php
+++ frontend/views/experience/index.php
@@ -64,7 +64,7 @@ $this->params['breadcrumbs'][] = $this->title;
          <div class="file-footer-caption">
             <h3><?= $tour['name'] ?> </h3>
             <div><?= substr(strip_tags($tour['overview']), 0, 120)  ?>...</div>
-            <div>
+            <div class="tourlist-price">
               <?php if(!empty($tour['price_cny'])) { ?>
                 From <span><?= Yii::$app->params['currency_name'][Yii::$app->params['currency']]['sign'] ?><?= number_format(common\models\ExchangeUsd::convertCurrency(Yii::$app->params['currency'], $tour['price_cny']),0) ?></span> <?= Yii::$app->params['currency_name'][Yii::$app->params['currency']]['name'] ?>
               <?php } ?>
