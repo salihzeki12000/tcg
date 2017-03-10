04fdeb4576ce0df8e8327f4991eb05b0234e0974
mixiaobin <mixiaobin@rong360ad.com>
2017-03-10 15:00:44 +0800

    up

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -25,7 +25,7 @@ AppAsset::register($this);
     <meta name="description" content="<?= $this->description ?>" />
     <meta name="keywords" content="<?= $this->keywords ?>" />
     <?php $this->head() ?>
-    <script type="text/javascript"> //<![CDATA[ 
+    <script async type="text/javascript"> //<![CDATA[ 
         var tlJsHost = ((window.location.protocol == "https:") ? "https://secure.comodo.com/" : "http://www.trustlogo.com/");
         document.write(unescape("%3Cscript src='" + tlJsHost + "trustlogo/javascript/trustlogo.js' type='text/javascript'%3E%3C/script%3E"));
         //]]>
