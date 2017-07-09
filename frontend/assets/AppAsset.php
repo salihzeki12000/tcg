39be29d96a2c11973c4a079f9b55e5f21b4c0ea9
mixiaobin <mixiaobin@rong360ad.com>
2017-07-09 21:35:09 +0800

    up

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -12,7 +12,7 @@ class AppAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        'statics/css/site.css?_t=201706300001',
+        'statics/css/site.css?_t=201707090001',
     ];
     public $js = [
         // 'statics/js/jgestures.min.js',
