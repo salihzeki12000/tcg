8c250fd00c04c7ba9244c9fa23b393c324311d2f
micklests <mixiaobin@gmail.com>
2017-10-22 22:05:41 +0800

    Update AppAsset.php

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -12,7 +12,7 @@ class AppAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        'statics/css/site.css?_t=201710220001',
+        'statics/css/site.css?_t=201710220002',
     ];
     public $js = [
         // 'statics/js/jgestures.min.js',
