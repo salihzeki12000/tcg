cf629cd01a2875d922dcea01a56c2f8949a94378
PC <pedro@thechinaguide.com>
2017-11-07 15:56:58 +0800

    style: Increase margin pages
    This update increases the left and right margin of the blog index and articles.

--- frontend/views/article/index.php
+++ frontend/views/article/index.php
@@ -25,7 +25,7 @@ $this->params['breadcrumbs'][] = Yii::t('app','Blog');
   </div>
 </div>
 
-<div class="tour-view">
+<div class="tour-view blog">
   <div class="container tour-left col-lg-8 col-md-12 col-sm-12 col-xs-12">
 
     <div class="article-index blog">
