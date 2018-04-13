52884e6d295762adf3142c1ef4b6877d43286ee8
PC <pedro@thechinaguide.com>
2018-04-13 11:09:31 +0800

    feat: Bcc agents when sending a feedback request

--- console/controllers/TourController.php
+++ console/controllers/TourController.php
@@ -26,48 +26,54 @@ class TourController extends Controller
     
     public function actionSendfeedbackform()
     { 
-	    if(empty($this->tour_end_date)):
-	    	$this->tour_end_date = date('Y-m-d');
-	    endif;
-	    
-	    $clients = ArrayHelper::toArray(OaTour::find()->where(['tour_end_date' => $this->tour_end_date])->all(), [
-		    'common\models\OaTour' => [
-		        'id',
-		        'contact',
-		        'email',
-		        'language',
-		    ],
-	    ]);
-
-		foreach($clients as $client):
-			if($client['language'] == 'German'):
-				$language = 'de';
-				$subject = "Wie war Ihre Reise mit The China Guide?";
-			elseif($client['language'] == 'Spanish'):
-				$language = 'es';
-				$subject = "¿Cómo estuvo su viaje con The China Guide?";
-			elseif($client['language'] == 'French'):
-				$language = 'fr';
-				$subject = "Comment s'est passé votre voyage avec The China Guide?";
-			else:
-				$language = 'en';
-				$subject = "How was your trip with The China Guide?";
-			endif;
-			
-			$code = str_replace('-','',$this->tour_end_date).$client['id'];
-
-            Yii::$app->mailer->compose("request-feedback", [
-	            'contact' => explode(' ', trim($client['contact']))[0],
-	            'language' => $language,
-	            'code' => $code,
-            ])
-				->setFrom(["emily@thechinaguide.com" => "Emily France"])
-				->setReplyTo("feedback@thechinaguide.com")
-                ->setTo(trim($client['email']))
-                ->setBcc("feedback@thechinaguide.com")
-                ->setSubject($subject)
-                ->send();
-		endforeach;
+	    if(!empty($this->tour_end_date)):
+		    $clients = ArrayHelper::toArray(OaTour::find()->where(['tour_end_date' => $this->tour_end_date])->all(), [
+			    'common\models\OaTour' => [
+			        'id',
+			        'contact',
+			        'email',
+			        'language',
+			        'agent'
+			    ],
+		    ]);
+	
+			foreach($clients as $client):
+				if($client['language'] == 'German'):
+					$language = 'de';
+					$subject = "Wie war Ihre Reise mit The China Guide?";
+				elseif($client['language'] == 'Spanish'):
+					$language = 'es';
+					$subject = "¿Cómo estuvo su viaje con The China Guide?";
+				elseif($client['language'] == 'French'):
+					$language = 'fr';
+					$subject = "Comment s'est passé votre voyage avec The China Guide?";
+				else:
+					$language = 'en';
+					$subject = "How was your trip with The China Guide?";
+				endif;
+				
+				if(($agent = \common\models\User::findOne($client['agent'])) !== null) {
+					$agent_mail = $agent->email;
+            	} else {
+	            	$agent_mail = '';
+            	}
+            	
+            	$bcc = array('feedback@thechinaguide.com', $agent_mail);
+				$code = str_replace('-','',$this->tour_end_date).$client['id'];
+	
+	            Yii::$app->mailer->compose("request-feedback", [
+		            'contact' => explode(' ', trim($client['contact']))[0],
+		            'language' => $language,
+		            'code' => $code,
+	            ])
+					->setFrom(["emily@thechinaguide.com" => "Emily France"])
+					->setReplyTo("feedback@thechinaguide.com")
+	                ->setTo(trim($client['email']))
+	                ->setBcc($bcc)
+	                ->setSubject($subject)
+	                ->send();
+			endforeach;
+		endif;
     }
 }
 
